51176fcb9457d302de1f840cb05a3a2b2ea8bc89
Đoàn Ngọc Bình Minh <bmmit.work@gmail.com>
2025-07-26 04:10:58 +0700

    checkpoint: demo

--- src/app/staff/[stationCodename]/page.tsx
+++ src/app/staff/[stationCodename]/page.tsx
@@ -1,14 +1,13 @@
 import { use } from 'react'
 import { PinProtected } from '@/components/staff/PinProtected'
-import StationData from '@/lib/data/station'
 
-export async function generateStaticParams() {
-  const stations = await StationData.getAll()
-
-  return stations.map(station => ({
-    stationCodename: station.codename,
-  }))
-}
+// export async function generateStaticParams() {
+//   const stations = await StationData.getAll()
+//
+//   return stations.map(station => ({
+//     stationCodename: station.codename,
+//   }))
+// }
 
 export default function StationPage({ params }: { params: Promise<{ stationCodename: string }> }) {
   const { stationCodename } = use(params)
